bf4ab6b9e6538e3f1d6412895b002db8009d05c3
Justin McSpadden <j.mcspadden@yahoo.com>
2020-03-07 13:29:18 -0500

    update

---
+++ Resources/ELTschema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE Resturant_DataFrame (
+  id INT ,
+  license_no INT,
+  inspect_date VARCHAR
+  city TEXT,
+  state TEXT,
+  zip INT PRIMARY KEY
+  
+);
+
+CREATE TABLE Census (
+  Zipcode INT,
+  Population INT,
+  Median Age INT,
+  Household Income INT,
+  Per Capita Income INT,
+  Poverty Count INT, 
+  Poverty Rate INT,
+  Unemployment Rate INT,
+  
+);
+
+
+
+Zipcode	Population	Median Age	Household Income	Per Capita Income	Poverty Count	Poverty Rate	Unemployment Rate
